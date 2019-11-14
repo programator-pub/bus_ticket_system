5cec5b46037609313305a5e21050b43ae3ebd899
lovilogic <info@lovilogic.com>
2019-11-14 19:49:13 +0100

    Worker user data update feature - WIP

--- src/main/java/pl/connectis/programator/service/impl/WorkerServiceImpl.java
+++ src/main/java/pl/connectis/programator/service/impl/WorkerServiceImpl.java
@@ -1,4 +1,44 @@
 package pl.connectis.programator.service.impl;
 
-public class WorkerServiceImpl {
+import pl.connectis.programator.dao.DB;
+import pl.connectis.programator.model.Client;
+import pl.connectis.programator.model.Route;
+import pl.connectis.programator.model.Ticket;
+import pl.connectis.programator.service.WorkerService;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public class WorkerServiceImpl implements WorkerService {
+
+    @Override
+    public Client registerClient(String firstName, String secondName) {
+        return null;
+    }
+
+    @Override
+    public Client updateClientData(Client client, String firstName, String secondName) throws Exception {
+        Optional<Client> clientOptional = Optional.ofNullable(DB.getClientList().stream()
+                .filter(client1 -> client1.equals(client)).findFirst().get());
+
+        return clientOptional.get();
+
+    }
+
+    @Override
+    public Client sellTicketToClient(Client client, Ticket ticket) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Client> getAllClients() {
+        return null;
+    }
+
+    @Override
+    public Route addNewRoute(BigDecimal price, String start, String destination, LocalDateTime tripDuration) {
+        return null;
+    }
 }
